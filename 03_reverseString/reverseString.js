d4aea9681aa2fe9e593559bb1bca2173e02ed49e
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-01-05 21:35:38 -0800

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function(str) {
-
-};
-
+function reverseString(str){
+    let splitString = str.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
+}
+reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
